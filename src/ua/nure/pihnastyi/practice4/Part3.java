3f3aacc351fec015c11a43ba13a066f5db49c02a
ardier16 <ardier16@gmail.com>
2019-01-20 12:24:29 +0200

    Refactor Part3
    + fix formatting
    + rename constants
    + move magic strings to constants
    + rename variables
    + extract getRegexType method
    + extract getEntriesStringByRegexType method

--- src/ua/nure/pihnastyi/practice4/Part3.java
+++ src/ua/nure/pihnastyi/practice4/Part3.java
@@ -5,47 +5,67 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Part3 {
-    public static final String SEPARATOR = "\\S+";
-    public static final String REGEX_CHAR = "^\\S$";
-    public static final String REGEX_STRING = "[A-zА-я]{2,}";
-    public static final String REGEX_INT = "^(-|)?\\d+$";
-    public static final String REGEX_DOUBLE = "^(((-|)?\\d+\\.(\\d+|))|((-|)?\\.\\d+))$";
+    private static final String FILE_NAME = "part3.txt";
+    private static final String FILE_ENCODING = "Cp1251";
+    private static final String SYSTEM_ENCODING = "UTF-8";
 
+    public static final String SEPARATOR_REGEX = "\\S+";
+    public static final String CHARACTER_REGEX = "^\\S$";
+    public static final String STRING_REGEX = "[A-zА-я]{2,}";
+    public static final String INTEGER_REGEX = "^(-|)?\\d+$";
+    public static final String DOUBLE_REGEX = "^(((-|)?\\d+\\.(\\d+|))|((-|)?\\.\\d+))$";
 
     public static void main(String[] args) {
-        String inputLine = Util.readFile("part3.txt", "Cp1251");
-        Scanner in = new Scanner(System.in, "UTF-8");
+        String input = Util.readFile(FILE_NAME, FILE_ENCODING);
+        
+        Scanner in = new Scanner(System.in, SYSTEM_ENCODING);
+        String regexType;
+        String entriesString;
         while (in.hasNextLine()) {
-            System.out.println(getType(in.nextLine(), inputLine));
+            regexType = getRegexType(in.nextLine())
+            entriesString = getEntriesStringByRegexType(input, regexType);
+            System.out.println(entriesString);
         }
     }
 
-    public static String getType(String consoleIn, String input) {
-        String isType="";
-        if ("char".equals(consoleIn)) {
-            isType = REGEX_CHAR;
-        } else if ("String".equals(consoleIn)) {
-            isType = REGEX_STRING;
-        } else if ("int".equals(consoleIn)) {
-            isType = REGEX_INT;
-        } else if ("double".equals(consoleIn)) {
-            isType = REGEX_DOUBLE;
-
+    private static String getRegexType(String type) {
+        String regexType = "";
+        switch (type) {
+            case "char":
+                regexType = CHARACTER_REGEX;
+                break;
+            case "String":
+                regexType = STRING_REGEX;
+                break;
+            case "int":
+                regexType = INTEGER_REGEX;
+                break;
+            case "double":
+                regexType = DOUBLE_REGEX;
+                break;
+            default:
+                regexType = STRING_REGEX;
+                break;
         }
 
-        Pattern pattern = Pattern.compile(SEPARATOR);
-        Matcher m = pattern.matcher(input);
+        return regexType;
+    }
+    
+    private static void getEntriesStringByRegexType(String input, 
+            String regexType) {
+        Pattern pattern = Pattern.compile(SEPARATOR_REGEX);
+        Matcher matcher = pattern.matcher(input);
         StringBuilder sb = new StringBuilder();
 
-        while (m.find()) {
-            Matcher mType = Pattern.compile(isType).matcher(m.group());
+        while (matcher.find()) {
+            Matcher typedMatcher = Pattern.compile(regexType)
+                    .matcher(matcher.group());
 
-            if (mType.find()) {
-                sb.append(mType.group()).append(" ");
+            if (typedMatcher.find()) {
+                sb.append(typedMatcher.group()).append(" ");
             }
         }
+
         return sb.toString();
     }
-
-
 }
